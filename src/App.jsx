f20317a6853bcc696283872b5a97202589995e8f
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2023-03-12 11:38:45 -0400

    create components and logic game

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
+import "./index.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+// TURNOS DE JUGADORES
+const TURNS = {
+  x: 'x',
+  o: 'o'
+}
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div onClick={handleClick} className={className}>
+      {children}
     </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.x)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos las posiciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // no hay ganador 
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // si ya se jugo esa celda no se actualiza
+    if (board[index] || winner) return
+
+    // actualiza la celda
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // cambia el turno
+    const newTturn = turn === TURNS.x? TURNS.o : TURNS.x
+    setTurn(newTturn)
+
+    // verifica si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
+  
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          }) 
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.x}>
+          {TURNS.x}
+        </Square>
+        <Square isSelected={turn === TURNS.o}>
+          {TURNS.o}
+        </Square>
+      </section>
+    </main>
   )
 }
 
