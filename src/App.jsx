af5668444cad893aafe56572852718663c967472
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2023-03-12 12:01:27 -0400

    Create logic reset game and tie

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,16 @@ function App() {
     return null
   }
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.x)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null)
+  }
+
   const updateBoard = (index) => {
     // si ya se jugo esa celda no se actualiza
     if (board[index] || winner) return
@@ -73,12 +83,15 @@ function App() {
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
   }
   
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
         {
           board.map((_, index) => {
@@ -94,6 +107,7 @@ function App() {
           }) 
         }
       </section>
+
       <section className="turn">
         <Square isSelected={turn === TURNS.x}>
           {TURNS.x}
@@ -102,6 +116,31 @@ function App() {
           {TURNS.o}
         </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                  ? 'Empate'
+                  : 'Gano:'
+                }
+              </h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square> }
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
